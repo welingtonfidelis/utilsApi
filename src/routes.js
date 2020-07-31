739e1ef06db01fe40ad22498a536b5eb098e818a
Welington Fidelis <welington.fidelis@tech4h.com.br>
2020-07-31 19:47:39 -0300

    Rota de login incluida

--- src/routes.js
+++ src/routes.js
@@ -1,19 +1,31 @@
 const express = require('express')
 const routes = express.Router();
+const bcrypt = require('bcrypt');
 
 const auth = require('./services/auth');
 const sendMailNodeMailer = require('./services/sendMailNodeMailer');
 const sendMailSendgrid = require('./services/sendMailSendgrid');
 
-routes.use(auth.verifyJWT)
+const saltRounds = 10;
 
-routes.post('/sendmail/nodemailer', (req, res) => {
+routes.post('/login', async (req, res) => {
     try {
-        const { mailTo, message, subject } = req.body;
+        const { user, password } = req.body;
 
-        sendMailNodeMailer(mailTo, subject, message);
-    
-        res.send({ status: true });
+        const hash = process.env.USER_PASSWORD;
+        const userAccount = process.env.USER_ACCOUNT;
+
+        const isValid = await bcrypt.compareSync(password, hash);
+
+        if (user != userAccount || !isValid) {
+            throw {
+                code: 401,
+                message: "Invalid user or password"
+            }
+        }
+
+        const Authorization = auth.createToken();
+        res.send({ Authorization });
 
     } catch (error) {
         console.error('ERROR!!!', error);
@@ -26,7 +38,33 @@ routes.post('/sendmail/nodemailer', (req, res) => {
 
         const message = error.message || error.stack || error.errors || error;
 
-        res.send(code).send({status: false, message });
+        res.status(code).json({ status: false, message });
+    }
+})
+
+//Rotas com validação de token
+routes.use(auth.verifyJWT);
+
+routes.post('/sendmail/nodemailer', (req, res) => {
+    try {
+        const { mailTo, message, subject } = req.body;
+
+        sendMailNodeMailer(mailTo, subject, message);
+
+        res.json({ status: true });
+
+    } catch (error) {
+        console.error('ERROR!!!', error);
+        let code = 500;
+
+        if (error.code) {
+            code = error.code;
+            delete error.code;
+        }
+
+        const message = error.message || error.stack || error.errors || error;
+
+        res.status(code).json({ status: false, message });
     }
 });
 
@@ -35,8 +73,8 @@ routes.post('/sendmail/sendgrid', (req, res) => {
         const { mailFrom, mailTo, message, subject } = req.body;
 
         sendMailSendgrid(mailFrom, mailTo, subject, message);
-    
-        res.send({ status: true });
+
+        res.json({ status: true });
 
     } catch (error) {
         console.error('ERROR!!!', error);
@@ -49,7 +87,7 @@ routes.post('/sendmail/sendgrid', (req, res) => {
 
         const message = error.message || error.stack || error.errors || error;
 
-        res.send(code).send({status: false, message });
+        res.status(code).json({ status: false, message });
     }
 });
 
